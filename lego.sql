6b98e5809d8741ce2168a8556c8b20e380713e85
Jason Muteham <jason@jasonmuteham.com>
2023-03-01 01:07:28 +0000

    More SQL

--- lego.sql
+++ lego.sql
@@ -11,8 +11,6 @@ Create a report to summarize your findings. Include:
 5. [Optional] What proportion of Lego parts are transparent?
 6. [Optional] What are the 5 rarest lego bricks?
 7. Summarize your findings.
-
----
 */
 
 /*markdown
@@ -33,7 +31,13 @@ FROM sets
 
 SELECT year AS 'Release Year', COUNT(*) AS 'Sets Released'
   FROM sets
-WHERE year BETWEEN '2010' AND '2020'
+--WHERE year BETWEEN '2010' AND '2020'
+GROUP BY year 
+ORDER By year DESC;
+
+SELECT year AS 'Release Year', COUNT(*) AS 'Sets Released'
+  FROM sets
+WHERE year BETWEEN '2010' AND '2022'
 GROUP BY year 
 ORDER By year;
 
@@ -50,70 +54,65 @@ SELECT SUM(num_parts) AS 'Total Parts',
        SUM(num_parts) / (MAX(year) - MIN(year))  AS 'Avg parts per year'
 FROM sets
 
-SELECT *
-FROM sets
-WHERE set_num = '5004559-1'
-
-SELECT *
-FROM inventories
-WHERE set_num = '5004559-1'
-
-SELECT *
-FROM inventory_sets
-WHERE inventory_id = '35'
-
-SELECT *
-FROM sets
-WHERE set_num IN ('75912-1','75911-1')
-
-SELECT *
-FROM sets
-WHERE set_num = '8088-1'
-
-SELECT *
-FROM inventories
-WHERE set_num = '8088-1'
-
-SELECT *
-FROM inventory_sets
-WHERE inventory_id IN ('1700','23731')
-
-SELECT *
-FROM sets
-WHERE set_num = 'K3433-1'
-
-SELECT *
-FROM inventories
-WHERE set_num = "K3433-1"
-
-SELECT *
-FROM inventory_sets
-WHERE inventory_id = '14222'
-
-SELECT *
-FROM sets
-WHERE set_num IN ('10121-1','3433-1')
-
-SELECT *
-FROM inventory_sets
-WHERE set_num IN ('10121-1','3433-1')
-
 /*markdown
-![rebrickable schema](https://rebrickable.com/static/img/diagrams/downloads_schema_v3.png)
+**Q. Top 10 sets with the most parts.**
 */
 
-SELECT COUNT(*) AS 'Number of Sets'
+SELECT set_num, name, year, num_parts AS total_parts
+FROM sets
+ORDER by total_parts DESC
+LIMIT 10
+
+SELECT COUNT(sets.name) AS 'Total Sets', (SELECT COUNT(name) FROM minifigs) AS 'Total Mini figures'
 FROM sets
 
-SELECT COUNT(*) AS 'Number of Mini Figures'
-FROM minifigs
+SELECT year AS 'Year', SUM(num_sets) as 'Sets',SUM(tot_parts) AS 'No. Parts Required'
+FROM (
+SELECT s.year as year, SUM(1) as num_sets, SUM(s.num_parts) as tot_parts
+FROM inventories AS i
+INNER JOIN sets as s
+USING(set_num)
+WHERE s.year < 2023
+GROUP BY s.year
 
-SELECT COUNT(DISTINCT set_num)
-FROM inventory_sets
+UNION ALL
 
-SELECT COUNT(DISTINCT set_num)
-FROM inventories
-WHERE set_num IS NOT NULL
+SELECT ss.year as year, SUM(inv.quantity) as num_sets, SUM(inv.quantity * ss.num_parts) as tot_parts
+FROM inventory_sets AS inv
+INNER JOIN sets as ss
+USING(set_num)
+WHERE ss.year < 2023
+GROUP BY ss.year
+)
+GROUP BY year
+ORDER BY year DESC
 
-SELECT COUNT(set_num)
-FROM inventories
+SELECT SUM(num_sets) as 'Sets',SUM(tot_parts) AS 'No. Parts Required', SUM(tot_parts) / SUM(num_sets) AS 'Average Part per Set'
+FROM (
+SELECT SUM(1) as num_sets, SUM(s.num_parts) as tot_parts
+FROM inventories AS i
+INNER JOIN sets as s
+USING(set_num)
+WHERE s.year < 2023
+GROUP BY s.year
+
+UNION ALL
+
+SELECT SUM(inv.quantity) as num_sets, SUM(inv.quantity * ss.num_parts) as tot_parts
+FROM inventory_sets AS inv
+INNER JOIN sets as ss
+USING(set_num)
+WHERE ss.year < 2023
+GROUP BY ss.year
+)
+
+/*markdown
+**Q. How many Star Wars themed set are there?**
+*/
+
+SELECT t.name AS 'Theme Name', COUNT(s.set_num) AS 'Number of Sets'
+FROM sets AS s
+JOIN themes AS t
+ON s.theme_id = t.id
+WHERE t.name LIKE '%star wars%'
+GROUP BY t.name
