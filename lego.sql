cd5c6af323ae6894e420105671afcd6163fb63d1
Jason Muteham <jason@jasonmuteham.com>
2023-03-05 15:25:39 +0000

    Update

--- lego.sql
+++ lego.sql
@@ -4,13 +4,13 @@
 
 /*markdown
 Create a report to summarize your findings. Include:
-1. What is the average number of Lego sets released per year?
-2. What is the average number of Lego parts required for the sets released each year?
-3. Create a visualization for item 2.
-4. What are the 5 most popular colors used in Lego parts?
-5. [Optional] What proportion of Lego parts are transparent?
-6. [Optional] What are the 5 rarest lego bricks?
-7. Summarize your findings.
+1. What is the average number of Lego sets and parts released each year, excluding mini figures?
+2. If you wanted to make every set released how many sets & parts would be required, include mini figures?
+3. Show the trend of sets & parts over time
+4. Top 10 sets with the most parts.
+5. How many Star Wars themed set are there?
+6. [Optional] What proportion of Lego parts are transparent?
+7. [Optional] What are the 5 rarest lego bricks?
 */
 
 /*markdown
@@ -21,22 +21,20 @@ Schema & data from [rebrickable](https://rebrickable.com)
 */
 
 /*markdown
-**Q. What is the average number of Lego sets and parts released each year?**
+**Q. What is the average number of Lego sets and parts released each year, excluding mini figures?**
 */
 
---These results are a lower than reality as sets can made up of multiplies of other sets.
 SELECT COUNT(*) AS 'Total Sets',
        MAX(year) - MIN(year) AS 'Years Est',
        COUNT(year) / (MAX(year) - MIN(year))  AS 'Avg Sets per Year',
        SUM(num_parts) AS 'Total Parts',
-       MAX(year) - MIN(year) AS 'Years Est',
        SUM(num_parts) / (MAX(year) - MIN(year))  AS 'Avg Parts per Year'
 FROM sets
 
 /*markdown
-**Q. If you wanted to make every set released how many sets & parts would be required?**
+**Q. If you wanted to make every set released how many sets & parts would be required, include mini figures?**
 
-Note: Sets can contain multiples of other sets we are assuming that reusing sets is not allowed!
+Note: Sets can contain multiples of other sets, we are assuming that reusing sets is not allowed!
 */
 
 --Set can be made up of parts, other sets or both so we need to gather the data from a few tables.
@@ -70,7 +68,11 @@ INNER JOIN minifigs as m
 USING(fig_num)
 )
 
-SELECT year AS 'Release Year', COUNT(*) AS 'Sets Released'
+/*markdown
+**Q. Show the trend of sets & parts over time.** 
+*/
+
+SELECT year AS 'Release Year', COUNT(*) AS 'Sets Released', SUM(num_parts) AS 'Parts Required'
   FROM sets
 WHERE year BETWEEN '2010' AND '2022'
 GROUP BY year 
@@ -85,7 +87,7 @@ ORDER By year;
 */
 
 /*markdown
-The number of parts in a set can be calculate by two methods.  The **sets** table has a field called **num_parts**. Alternatively the inventory_parts table can be aggregated. The results between the methods highlight a small difference in the number of parts, this discrepancy is possibly due to the **sets** table **num_parts** field not being updated correctly? Despite the difference the top 10 is the same.  
+The number of parts in a set can be calculate by two methods.  The **sets** table has a field called **num_parts**. Alternatively the inventory_parts table can be aggregated. The results between the methods highlight a small difference in the number of parts, this discrepancy is possibly due to the **sets** table **num_parts** field not being updated correctly? Despite the difference the top 10 is the same. 
 */
 
 SELECT set_num AS set_number, name AS 'Name', year AS 'Year', num_parts AS Total_Parts
@@ -130,6 +132,7 @@ USING(set_num)
 WHERE ss.year < 2023
 GROUP BY ss.year
 )
+WHERE year BETWEEN '2010' AND '2022'
 GROUP BY year
 ORDER BY year DESC
 
